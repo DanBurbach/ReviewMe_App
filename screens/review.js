ed4f6ad85b4bfd8f0573ae021cb098a676dbdca5
DanBurbach <41841936+DanBurbach@users.noreply.github.com>
2019-12-16 14:39:40 -0800

    Initial Commit

---
+++ screens/review.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function Review() {
+    return (
+        <View style={StyleSheet.container}>
+            <Text>Review Screen</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 24
+    }
+});
