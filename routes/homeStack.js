415f306c9b560a14eba10a7a9ed6771ee6123843
DanBurbach <dburbach1982@gmail.com>
2019-12-22 23:12:54 -0800

    build basic routes for starter project

---
+++ routes/homeStack.js
@@ -0,0 +1,17 @@
+import { createStackNavigator } from 'react-navigation-stack';
+import { createAppContainer } from 'react-navigation';
+import Home from '../screens/home';
+import Review from '../screens/review';
+
+const screens = {
+    Home : {
+        screen: Home
+    },
+    Review: {
+        screen: Review
+    }
+}
+
+const HomeStack = createStackNavigator(screens);
+
+export default createAppContainer(HomeStack);
