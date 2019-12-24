0e48f11ca5a8eccf9442d2537a01e6e031c2f605
DanBurbach <dburbach1982@gmail.com>
2019-12-23 23:14:37 -0800

    add display of selected List Item through navigation. Update styles for data listing

--- screens/review.js
+++ screens/review.js
@@ -1,16 +1,48 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Button, TouchableOpacity } from 'react-native';
 
-export default function Review() {
+
+export default function Review({navigation}) {
+
+    const pressHomeHandler = () => {
+        navigation.goBack();
+    }
+
     return (
         <View style={styles.container}>
-            <Text>Review Screen</Text>
+            <Text style= {styles.TitleText}>Review Screen</Text>
+            <Text style= {styles.ReviewList}>{ navigation.getParam('title')}</Text>
+            <Text style= {styles.ReviewList}>{ navigation.getParam('rating')}</Text>
+            <Text style= {styles.ReviewList}>{ navigation.getParam('body')}</Text>
+            <TouchableOpacity
+                style={styles.HomeButton}>
+                <Button 
+                    title='Go back to Home Screen' 
+                    color = 'white'
+                    fontSize={18}
+                    fontFamily= 'girassol-regular'
+                    onPress={pressHomeHandler}
+                />
+            </TouchableOpacity>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-        padding: 24
+        paddingTop: 40,
+        padding: 24,
+    },
+    TitleText: {
+        fontFamily: 'girassol-regular',
+        fontSize: 36,
+    },
+    ReviewList: {
+        fontFamily: 'girassol-regular',
+        fontSize: 20,
+    },
+    HomeButton : {
+        backgroundColor: 'orange',
+        color: 'white',
     }
 });
