f96cea754e2d3f181fdd5f80573c455627b217bd
DanBurbach <dburbach1982@gmail.com>
2019-12-23 23:13:50 -0800

    add a FlatList and data to State, display and access through TouchableOpacity list with onPress to Review Page

--- screens/home.js
+++ screens/home.js
@@ -1,7 +1,14 @@
-import React from 'react';
-import { Button, View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { Button, View, Text, StyleSheet, TouchableOpacity, FlatList } from 'react-native';
 
 export default function Home({ navigation }) {
+
+    const [reviews, setReviews] = useState([
+        { title: 'Big Trouble in Little China', rating: 5, body: 'lorem ipsum', key: '1'},
+        { title: 'Plan 9 From Outer Space', rating: 1, body: 'lorem ipsum', key: '2'},
+        { title: '12 Monkeys', rating: 3, body: 'lorem ipsum', key: '3'},
+    ]);
+    
     const reviewPressHandler = () => {
         navigation.push('Review');
     }
@@ -9,10 +16,21 @@ export default function Home({ navigation }) {
     return (
         <View style={styles.container}>
             <Text style={styles.titleHomeText}>Home Screen</Text>
-            <TouchableOpacity style = {
-                    styles.ReviewButton
-            }>
-                <Button title='GotoReview' color='white' onPress={reviewPressHandler}/>
+            <FlatList data={reviews} renderItem={({ item }) => (
+                    <TouchableOpacity onPress={() => navigation.navigate('Review', item)}>
+                        <Text style={styles.titleText}>{ item.title }</Text>
+                    </TouchableOpacity>
+                )}
+            />
+            <TouchableOpacity 
+                style = {styles.ReviewButton}>
+                <Button 
+                    title = 'Go to Review'
+                    color = 'white'
+                    fontSize={18}
+                    fontFamily= 'girassol-regular'
+                    onPress = {reviewPressHandler}
+                />
             </TouchableOpacity>
         </View>
     )
@@ -27,8 +45,13 @@ const styles = StyleSheet.create({
         fontFamily: 'girassol-regular',
         fontSize: 36,
     },
+    titleText: {
+        fontFamily: 'girassol-regular',
+        fontSize: 20,
+        color: 'black'
+    },
     ReviewButton: {
         backgroundColor: 'orange',
-        color: 'white'
+        color: 'white',
     }
 });
