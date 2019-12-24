06bac05345e489a6d8ac332d648b5fd710143e27
DanBurbach <dburbach1982@gmail.com>
2019-12-23 23:22:40 -0800

    add to homeStack an object onto the HomeStack that uniformally sets the header styling, also add title onto navigationOptions for each of screens

--- routes/homeStack.js
+++ routes/homeStack.js
@@ -5,13 +5,23 @@ import Review from '../screens/review';
 
 const screens = {
     Home : {
-        screen: Home
+        screen: Home,
+        navigationOptions: {
+            title: "Film Review",
+        }
     },
     Review: {
-        screen: Review
+        screen: Review,
+        navigationOptions: {
+            title: "Review List",
+        }
     }
 }
 
-const HomeStack = createStackNavigator(screens);
+const HomeStack = createStackNavigator(screens, {defaultNavigationOptions: {
+    headerTintColor: '#444',
+    headerStyle: { backgroundColor: '#eee', height: 40}
+}
+});
 
 export default createAppContainer(HomeStack);
